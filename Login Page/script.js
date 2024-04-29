74ac26c61fc9ded6d2e6c3c80f9ead0d6221800c
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-29 21:01:58 +0300

    add variables

--- Login Page/script.js
+++ Login Page/script.js
@@ -1,4 +1,6 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 });
 
