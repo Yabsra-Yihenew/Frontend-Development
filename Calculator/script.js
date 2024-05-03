80e6eef4d91cdbd71fcdc64eb5e64e633a4dbc6c
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-05-03 20:07:38 +0300

    adding function that calculate result

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,3 +4,10 @@ function appendToResult(value) {
 function clearResult() {
     document.getElementById("result").value = "";
 }
+function calculate() {
+    try {
+        document.getElementById("result").value = eval(document.getElementById("result").value);
+    } catch (error) {
+        document.getElementById("result").value = "Error";
+    }
+}
