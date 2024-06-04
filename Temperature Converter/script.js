fdb8d7dc607cd25c1cd30067640bee0b178f6997
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-06-04 22:22:05 +0300

    Update script.js

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -6,13 +6,28 @@ function convertTemperature() {
                 document.getElementById('result').innerText = "Please enter a valid number.";
                 return;
             }
+    
         let tempInCelsius;
 
-    // Convert input temperature to Celsius
-    if (inputUnit === 'Celsius') {
-        tempInCelsius = inputValue;
-    } else if (inputUnit === 'Fahrenheit') {
-        tempInCelsius = (inputValue - 32) * 5 / 9;
-    } else if (inputUnit === 'Kelvin') {
-        tempInCelsius = inputValue - 273.15;
-    }
+        // Convert input temperature to Celsius
+        if (inputUnit === 'Celsius') {
+            tempInCelsius = inputValue;
+        } else if (inputUnit === 'Fahrenheit') {
+            tempInCelsius = (inputValue - 32) * 5 / 9;
+        } else if (inputUnit === 'Kelvin') {
+            tempInCelsius = inputValue - 273.15;
+        }
+    
+        let convertedTemp;
+        
+            // Convert Celsius temperature to the desired output unit
+            if (outputUnit === 'Celsius') {
+                convertedTemp = tempInCelsius;
+            } else if (outputUnit === 'Fahrenheit') {
+                convertedTemp = (tempInCelsius * 9 / 5) + 32;
+            } else if (outputUnit === 'Kelvin') {
+                convertedTemp = tempInCelsius + 273.15;
+            }
+        
+            document.getElementById('result').innerText = `Converted Temperature: ${convertedTemp.toFixed(2)} ${outputUnit}`;
+}
