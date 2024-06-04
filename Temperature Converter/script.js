5260aa2f62a0d49c05e8511498c3f0f3f1e8ddb8
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-06-04 22:20:33 +0300

    Update script.js

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -2,3 +2,7 @@ function convertTemperature() {
     const inputValue = parseFloat(document.getElementById('inputValue').value);
     const inputUnit = document.getElementById('inputUnit').value;
     const outputUnit = document.getElementById('outputUnit').value;
+         if (isNaN(inputValue)) {
+                document.getElementById('result').innerText = "Please enter a valid number.";
+                return;
+            }
