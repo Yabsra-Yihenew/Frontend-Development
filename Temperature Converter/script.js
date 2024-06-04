297e1976fcc27a7c28dfea4797aaa100ffcfd5f6
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-06-04 22:20:07 +0300

    Update script.js

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -1,1 +1,4 @@
-
+function convertTemperature() {
+    const inputValue = parseFloat(document.getElementById('inputValue').value);
+    const inputUnit = document.getElementById('inputUnit').value;
+    const outputUnit = document.getElementById('outputUnit').value;
