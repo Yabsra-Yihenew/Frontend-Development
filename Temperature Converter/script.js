baae255118665c4de55b209ca4e0a9ffcae934cc
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-06-06 21:46:18 +0300

    Update script.js

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -1,33 +1,9 @@
 function convertTemperature() {
-    const inputValue = parseFloat(document.getElementById('inputValue').value);
-    const inputUnit = document.getElementById('inputUnit').value;
-    const outputUnit = document.getElementById('outputUnit').value;
-         if (isNaN(inputValue)) {
-                document.getElementById('result').innerText = "Please enter a valid number.";
-                return;
-            }
-    
-        let tempInCelsius;
+    const temperatureInput = document.getElementById('temperatureInput').value;
+    const temperature = parseFloat(temperatureInput);
+    const scale = document.getElementById('scaleInput').value;
 
-        // Convert input temperature to Celsius
-        if (inputUnit === 'Celsius') {
-            tempInCelsius = inputValue;
-        } else if (inputUnit === 'Fahrenheit') {
-            tempInCelsius = (inputValue - 32) * 5 / 9;
-        } else if (inputUnit === 'Kelvin') {
-            tempInCelsius = inputValue - 273.15;
-        }
-    
-        let convertedTemp;
-        
-            // Convert Celsius temperature to the desired output unit
-            if (outputUnit === 'Celsius') {
-                convertedTemp = tempInCelsius;
-            } else if (outputUnit === 'Fahrenheit') {
-                convertedTemp = (tempInCelsius * 9 / 5) + 32;
-            } else if (outputUnit === 'Kelvin') {
-                convertedTemp = tempInCelsius + 273.15;
-            }
-        
-            document.getElementById('result').innerText = `Converted Temperature: ${convertedTemp.toFixed(2)} ${outputUnit}`;
-}
+    if (isNaN(temperature)) {
+        alert('Please enter a valid number');
+        return;
+    }
