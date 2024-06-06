5ea1087751f5f06bda3e7eaa49f3a4ebd89879d4
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-06-06 21:46:35 +0300

    Update script.js

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -7,3 +7,19 @@ function convertTemperature() {
         alert('Please enter a valid number');
         return;
     }
+    let result;
+
+    switch (scale) {
+        case 'C':
+            result = `${temperature.toFixed(2)} °C is equal to ${(temperature * 9 / 5 + 32).toFixed(2)} °F and ${(temperature + 273.15).toFixed(2)} K`;
+            break;
+        case 'F':
+            result = `${temperature.toFixed(2)} °F is equal to ${((temperature - 32) * 5 / 9).toFixed(2)} °C and ${(((temperature - 32) * 5 / 9) + 273.15).toFixed(2)} K`;
+            break;
+        case 'K':
+            result = `${temperature.toFixed(2)} K is equal to ${(temperature - 273.15).toFixed(2)} °C and ${((temperature - 273.15) * 9 / 5 + 32).toFixed(2)} °F`;
+            break;
+    }
+
+    document.getElementById('resultOutput').value = result;
+}
